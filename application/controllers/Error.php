b27ec12cc481c87b8cc46b4fe5ff9745c42f964e
XTuOne-Win8 <XTuOne-Win8@Fire>
2015-04-28 15:35:37 +0800

    Merge origin/master
    Conflicts:
    	.htaccess
    	controller/index/C_Index.php
    	index.php
    	init.php
    	model/M_City.php
    	nbproject/project.xml
    	trunk/componment/captcha/Com_Captcha.php
    	trunk/componment/city/Com_City.php
    	trunk/config/DB_config.php
    	trunk/config/Router_config.php
    	trunk/config/SMS_config.php
    	trunk/config/Smarty_config.php
    	trunk/config/Website_config.php
    	trunk/core/C_Control.php
    	trunk/core/Helper.class.php
    	trunk/core/M_Model.pdo.php
    	trunk/core/Orange.class.php
    	trunk/function/F_Basic.php
    	trunk/function/F_Cookie.php
    	trunk/function/F_Network.php
    	trunk/function/F_String.php
    	trunk/library/L_SMS.class.php
    	view/index.html

--- application/controllers/Error.php
+++ application/controllers/Error.php
@@ -6,18 +6,20 @@
 class ErrorController extends Yaf_Controller_Abstract {
 
 	public function errorAction($exception){
-        switch ($exception->getCode()) {
-            case YAF_ERR_NOTFOUND_MODULE:
-            case YAF_ERR_NOTFOUND_CONTROLLER:
-            case YAF_ERR_NOTFOUND_ACTION:
-            case YAF_ERR_NOTFOUND_VIEW:
-                echo 404, ":", $exception->getMessage();
-            break;
+        if(ENVIRONMENT == 'DEV'){ // only display errors under DEV
+            switch ($exception->getCode()) {
+                case YAF_ERR_NOTFOUND_MODULE:
+                case YAF_ERR_NOTFOUND_CONTROLLER:
+                case YAF_ERR_NOTFOUND_ACTION:
+                case YAF_ERR_NOTFOUND_VIEW:
+                    echo 404, ":", $exception->getMessage();
+                break;
 
-            default :
-                $message = $exception->getMessage();
-                echo 0, ":", $exception->getMessage();
-            break;
+                default :
+                    $message = $exception->getMessage();
+                    echo 0, ":", $exception->getMessage();
+                break;
+            }
         }
 	}
 
