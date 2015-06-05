ff8113109746dda6270dc4109a36e3aa816d7677
root <root@localhost.(none)>
2015-06-05 22:02:59 +0800

    Seperate login and register

---
+++ application/controllers/Login.php
@@ -0,0 +1,41 @@
+<?php
+
+class LoginController extends BasicController {
+
+	private $m_user;
+
+	private function init(){
+		$this->m_user = $this->load('user');
+		$userID = $this->getSession('userID');
+
+		if($userID){
+			jsRedirect('/user/profile');
+		}
+	}
+
+	public function indexAction() {
+        
+  	}
+
+  	public function loginActAction(){
+		$username = $this->getPost('username');
+		$password = $this->getPost('password');
+
+		$field = array('id');
+		$where = array('username' => $username, 'password' => $password);
+		$data  = $this->m_user->Field($field)->Where($where)->SelectOne();
+		$userID = $data['id'];
+
+		if($userID){
+			// Set to session
+			$this->setSession('userID', $userID);
+			$this->setSession('username', $username);
+
+			$this->redirect('/user/profile'); // 会令 jsAlert失效
+		}else{
+			jsAlert('登录失败, 请检查用户名和密码');
+			jsRedirect('/login');
+		}
+	}
+  	
+}
