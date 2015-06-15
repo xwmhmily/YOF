fa94ece389b66b9abd62e3785a594dff11677f07
root <root@localhost.(none)>
2015-06-15 23:31:48 +0800

    Add api, http functions

--- application/modules/Api/controllers/Article.php
+++ application/modules/Api/controllers/Article.php
@@ -5,6 +5,8 @@ class ArticleController extends BasicController {
 	private $m_article;
 
 	private function init(){
+		// Verify sign
+		$this->verifySign(); 
 		$this->m_article = $this->load('Article');
 	}
 
@@ -14,8 +16,19 @@ class ArticleController extends BasicController {
 	}
 
 	public function detailAction(){
-		$articleID = $this->getQuery('articleID');
-		echo $articleID; die;
+		$articleID = intval($this->getPost('articleID'));
+		
+		// articleID is required, or we could do nothing !
+		if(!$articleID){
+			$this->response('ERR_MISSING');
+		}
+
+		$article = $this->m_article->SelectByID('', $articleID);
+
+		$rep['code'] = 1;
+		$rep['article'] = $article;
+
+		Helper::response($rep);
 	}
 	
 }
