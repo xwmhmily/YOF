fc5ecb177a138a97bdc1d4ea8accf5ff6f145b6d
root <root@localhost.(none)>
2015-06-15 23:31:22 +0800

    Add api, http functions

--- application/library/C_Basic.php
+++ application/library/C_Basic.php
@@ -49,4 +49,75 @@ class BasicController extends Yaf_Controller_Abstract {
     return Helper::load($model);
   }
 
+  /**
+   * Verify API sign
+   */
+  public function verifySign(){
+    $sign = $this->getRequest()->getPost('sign');
+    $i['time'] = $this->getRequest()->getPost('time');
+
+    // Only valid in 30 seconds
+    if(ENVIRONMENT != 'DEV'){
+      if(CUR_TIMESTAMP - $i['time'] > 30){
+        $rep['code']  = 1001;
+        $rep['error'] = 'error sign';
+
+        Helper::response($rep);
+      }
+    }
+
+    $newSign = Helper::generateSign($i);
+
+    if(strtolower($newSign) != $sign){
+      $rep['code']  = 1001;
+      $rep['error'] = 'error sign';
+
+      Helper::response($rep);
+    }
+  }
+
+  /*
+   *  Response
+   */
+  public function response($error){
+    switch($error){
+      case 'ERR_MISSING':
+        $rep['code'] = 1002;
+        $rep['error'] = 'misssing parameters';
+      break;
+
+      case 'ERR_NO_DATA':
+        $rep['code']  = 9998;
+        $rep['error'] = 'no data';
+      break;
+
+      case 'ERR_UNKNOWN':
+        $rep['code']  = 9999;
+        $rep['error'] = 'unknown error';
+      break;
+
+      case 'ERR_DUPLICATED':
+        $rep['code']  = 1110;
+        $rep['error'] = 'operation duplicated';
+      break;
+
+      case 'ERR_FAIL_UPLOAD_TO_CLOUD':
+        $rep['code'] = 1008;
+        $rep['error'] = 'failed to upload to cloud';
+      break;
+
+      case 'ERR_FAIL_UPLOAD_TO_TMP':
+        $rep['code'] = 1007;
+        $rep['error'] = 'failed to upload to tmp';
+      break;
+
+      case 'ERR_NO_FILE_RECEIVED':
+        $rep['code'] = 1009;
+        $rep['error'] = 'empty file received';
+      break;
+    }
+
+    Helper::response($rep);
+  }
+
 }
