b786e76054c9ed6ff14bb49162854898baf6fbd6
System Administrator <root@userdeMacBook-Pro.local>
2015-06-24 00:23:44 +0800

    Add qiniu class

---
+++ public/common/qiniu.php
@@ -0,0 +1,31 @@
+<?php
+
+$avatar = $_FILES['avatar'];
+
+if($avatar['name']){
+    Yaf_Loader::import('L_Upload.class.php');
+    
+    $dir = TMP_PATH.'/';
+            
+    $fileName = date('YmdHis');
+    $upload = new L_Upload($avatar, $dir);
+
+    $code = $upload->upload($fileName);
+
+    if($code){
+		Yaf_Loader::import('L_Qiniu.class.php');
+
+		$img = $fileName . '.' . $upload->extension;
+        $key = time().'.'.$upload->extension;
+        $source = $dir.$img;
+
+        $l_qiniu = new L_Qiniu();
+        list($ret, $err) = $l_qiniu->upload($key, $source);
+
+        if($err !== NULL){
+        	// Handle this error
+        }else{
+        	// Your code here ....
+        	unlink($source);
+        }
+    }
