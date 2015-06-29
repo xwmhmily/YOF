491933f7115ceff86fd440fa15ca2de904355ef0
System Administrator <root@userdeMacBook-Pro.local>
2015-06-29 22:39:28 +0800

    Fix a bug

--- application/modules/User/controllers/Profile.php
+++ application/modules/User/controllers/Profile.php
@@ -181,12 +181,12 @@ class ProfileController extends BasicController {
 		$url = $this->get('url', FALSE);
 		if($url){
 			// 此处为了演示和接近实际环境，将 URL 定死
-			$url = 'http://yof.mylinuxer.com/api/article';
+			//$url = 'http://yof.mylinuxer.com/api/article';
+			$url = 'http://www.yuefenqi.com/appapi/login';
 
 			// Secure your API with CUR_TIMESTAMP and API_KEY
 			$m['time'] = CUR_TIMESTAMP;
 			$m['sign'] = Helper::generateSign($m);
-
 			$buffer['content'] = executeHTTPRequest($url, $m); 
 		}
 
