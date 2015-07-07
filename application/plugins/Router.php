d081b9f939251d5aab41e0f931040bfd4f1eecbf
System Administrator <root@userdeMacBook-Pro.local>
2015-07-07 15:54:24 +0800

    Add Yaf_Route_Regex DEMO

--- application/plugins/Router.php
+++ application/plugins/Router.php
@@ -8,38 +8,30 @@ class RouterPlugin extends Yaf_Plugin_Abstract {
 
     // 去掉 Module 后的 index
     public function routerShutdown(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response) {
-    	//pr($request);
-
+        //pr($request);
+        
         $modules = Yaf_Application::app()->getModules();
 
     	$uri = $request->getRequestUri();
     	$uriInfo = explode('/', $uri);
 
-    	$module     = ucfirst($uriInfo[1]);
-    	$controller = $uriInfo[2];
-    	$action     = $uriInfo[3];
+    	$module = ucfirst($uriInfo[1]);
 
     	if(!in_array($module, $modules)){
             $module = 'index';
-            $controller = $uriInfo[1];
-            $action     = $uriInfo[2];
-    	}
+    	}else{
+            $request->setModuleName(ucfirst($module));
+            $request->setControllerName(ucfirst($uriInfo[2]));
 
-    	$request->setModuleName(ucfirst($module));
+            $action = ucfirst($uriInfo[3]);
+            if(!$action){
+                $action = 'index';
+            }
 
-    	if(!$controller){
-            $controller = 'index';
-    	}
+            $request->setActionName($action);
+        }
 
-    	$request->setControllerName(ucfirst($controller));
-
-    	if(!$action){
-            $action = 'index';
-    	}
-
-    	$request->setActionName($action);
-
-	   //pr($request);
+	   // pr($request);
     }
 
 }
