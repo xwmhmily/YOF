064da682d015c9dd1ea555ff818745836e0448b9
System Administrator <root@userdeMacBook-Pro.local>
2015-07-09 14:57:52 +0800

    Remove routerShutdwon function

--- application/plugins/Admin.php
+++ application/plugins/Admin.php
@@ -2,14 +2,6 @@
 
 class AdminPlugin extends Yaf_Plugin_Abstract {
 
-    public function routerStartup(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response) {
-
-    }
-
-    public function routerShutdown(Yaf_Request_Abstract $request, Yaf_Response_Abstract $response) {
-    	
-    }
-
     public function checkLogin(){
         $adminID = Yaf_Session::getInstance()->__get('adminID');
 
