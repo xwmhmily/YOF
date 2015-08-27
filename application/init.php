0ed299281ba497da11b78e37d987d055576264a1
System Administrator <root@userdeMacBook-Pro.local>
2015-08-27 23:44:01 +0800

    Ingore undefined notice

--- application/init.php
+++ application/init.php
@@ -62,7 +62,7 @@ define('SITE_PROVINCE', 440000);
 define('SITE_CITY',     440100);
 define('SITE_REGION',   440106);
 
-define('TMP_PATH', APP_PATH.'/tmp');
+define('TMP_PATH',    APP_PATH.'/tmp');
 define('UPLOAD_PATH', APP_PATH.'/public/upload');
 
 /*
@@ -72,215 +72,214 @@ define('UPLOAD_PATH', APP_PATH.'/public/upload');
  *     若有更好办法, 请告知, 谢谢!
  */
 function yofErrorHandler($errno, $errstr, $errfile, $errline, $sql = ''){
+    // 不理会 Undefined
+    if($errno == 8 || $errno== 2){
+        return TRUE;
+    }
+
     if(ENV != 'DEV'){
-        if($errno != 8){
-            if(!file_exists(LOG_FILE)){
-                touch(LOG_FILE);
-            }
-
-            file_put_contents(LOG_FILE, CUR_DATETIME.' '.$errno.PHP_EOL, FILE_APPEND);
-            file_put_contents(LOG_FILE, CUR_DATETIME.' '.$errstr.PHP_EOL, FILE_APPEND);
-            file_put_contents(LOG_FILE, CUR_DATETIME.' '.$errfile.PHP_EOL, FILE_APPEND);
-            file_put_contents(LOG_FILE, CUR_DATETIME.' '.$errline.PHP_EOL, FILE_APPEND);
-            
-            header('HTTP/1.1 500 Internal Server Error');
-            $html = '<html>
-                <head><title>500 Internal Server Error</title></head>
-                <body bgcolor="white">
-                <center><h1>500 Internal Server Error</h1></center>
-                <hr>
-                </body>
-            </html>';
-            die($html);
+        if(!file_exists(LOG_FILE)){
+            touch(LOG_FILE);
         }
+
+        file_put_contents(LOG_FILE, CUR_DATETIME.' '.$errno.PHP_EOL, FILE_APPEND);
+        file_put_contents(LOG_FILE, CUR_DATETIME.' '.$errstr.PHP_EOL, FILE_APPEND);
+        file_put_contents(LOG_FILE, CUR_DATETIME.' '.$errfile.PHP_EOL, FILE_APPEND);
+        file_put_contents(LOG_FILE, CUR_DATETIME.' '.$errline.PHP_EOL, FILE_APPEND);
+        
+        header('HTTP/1.1 500 Internal Server Error');
+        $html = '<html>
+            <head><title>500 Internal Server Error</title></head>
+            <body bgcolor="white">
+            <center><h1>500 Internal Server Error</h1></center>
+            <hr>
+            </body>
+        </html>';
+        die($html);
     }else{
-        // 不要 Undefined 错误
-        if($errno != 8){
-            $error = '<link href="CSS_PATH/bootstrap.min.css" rel="stylesheet">
-                <link href="CSS_PATH/bootstrap-responsive.min.css" rel="stylesheet">
-                <link href="CSS_PATH/docs.css" rel="stylesheet">
-                <script src="JS_PATH/jquery-1.7.min.js"></script>';
-            
-            $error .= '<style>
-                    body{
-                            font-family:"ff-tisa-web-pro-1","ff-tisa-web-pro-2","Lucida Grande","Helvetica Neue",Helvetica,Arial,"Hiragino Sans GB","Hiragino Sans GB W3","Microsoft YaHei UI","Microsoft YaHei","WenQuanYi Micro Hei",sans-serif;
-                            padding: 10px;
-                        }
-                    </style>';
-            $error .= '<link href="CSS_PATH/prettify.css" rel="stylesheet">';
-            $error .= "<script> 
-                      $(function(){
-                        $('#errorTab a').click(function(e){
-                            e.preventDefault();
-                            $('#errorTab a').parent().removeClass('active'); 
-                            $(this).parent().addClass('active');
+        $error = '<link href="CSS_PATH/bootstrap.min.css" rel="stylesheet">
+            <link href="CSS_PATH/bootstrap-responsive.min.css" rel="stylesheet">
+            <link href="CSS_PATH/docs.css" rel="stylesheet">
+            <script src="JS_PATH/jquery-1.7.min.js"></script>';
+        
+        $error .= '<style>
+                body{
+                        font-family:"ff-tisa-web-pro-1","ff-tisa-web-pro-2","Lucida Grande","Helvetica Neue",Helvetica,Arial,"Hiragino Sans GB","Hiragino Sans GB W3","Microsoft YaHei UI","Microsoft YaHei","WenQuanYi Micro Hei",sans-serif;
+                        padding: 10px;
+                    }
+                </style>';
+        $error .= '<link href="CSS_PATH/prettify.css" rel="stylesheet">';
+        $error .= "<script> 
+                  $(function(){
+                    $('#errorTab a').click(function(e){
+                        e.preventDefault();
+                        $('#errorTab a').parent().removeClass('active'); 
+                        $(this).parent().addClass('active');
 
-                            // 切换 DIV
-                            $('.tab-content div').removeClass('active');
-                            var id = $(this).attr('val');
-                            $('#'+id).addClass('active');
-                        }) 
-                      }) 
-                    </script>";
-            $error .= '<h4>Error : [ERROR_DESC]</h4>
-                        <ul class="nav nav-tabs" id="errorTab"> 
-                          <li class="active"><a val="general" href="#general">General</a></li> 
-                          <li><a val="request" href="#request">Request</a></li> 
-                          <li><a val="router" href="#router">Router</a></li>
-                          <li><a val="modules" href="#modules">Modules</a></li>
-                          <li><a val="config" href="#config">Config</a></li> 
-                          <li><a val="get" href="#get">GET</a></li>
-                          <li><a val="post" href="#post">POST</a></li> 
-                          <li><a val="cookie" href="#cookie">COOKIE</a></li> 
-                          <li><a val="session" href="#session">SESSION</a></li> 
-                          <li><a val="server" href="#server">SERVER</a></li>
-                          <li><a val="sql" href="#sql">SQL</a></li>
-                        </ul>';
+                        // 切换 DIV
+                        $('.tab-content div').removeClass('active');
+                        var id = $(this).attr('val');
+                        $('#'+id).addClass('active');
+                    }) 
+                  }) 
+                </script>";
+        $error .= '<h4>Error : [ERROR_DESC]</h4>
+                    <ul class="nav nav-tabs" id="errorTab"> 
+                      <li class="active"><a val="general" href="#general">General</a></li> 
+                      <li><a val="request" href="#request">Request</a></li> 
+                      <li><a val="router" href="#router">Router</a></li>
+                      <li><a val="modules" href="#modules">Modules</a></li>
+                      <li><a val="config" href="#config">Config</a></li> 
+                      <li><a val="get" href="#get">GET</a></li>
+                      <li><a val="post" href="#post">POST</a></li> 
+                      <li><a val="cookie" href="#cookie">COOKIE</a></li> 
+                      <li><a val="session" href="#session">SESSION</a></li> 
+                      <li><a val="server" href="#server">SERVER</a></li>
+                      <li><a val="sql" href="#sql">SQL</a></li>
+                    </ul>';
 
-            $error .= '<div class="tab-content">
-                          <div class="tab-pane active" id="general">[GENERAL_ERR]</div> 
-                          <div class="tab-pane" id="request">[REQUEST_ERR]</div> 
-                          <div class="tab-pane" id="router">[ROUTER_ERR]</div> 
-                          <div class="tab-pane" id="modules">[MODULES_ERR]</div>
-                          <div class="tab-pane" id="config">[CONFIG_ERR]</div>
-                          <div class="tab-pane" id="get">[GET_ERR]</div>
-                          <div class="tab-pane" id="post">[POST_ERR]</div>
-                          <div class="tab-pane" id="cookie">[COOKIE_ERR]</div>
-                          <div class="tab-pane" id="session">[SESSION_ERR]</div>
-                          <div class="tab-pane" id="server">[SERVER_ERR]</div>
-                          <div class="tab-pane" id="sql">[SQL_ERR]</div>
-                        </div>';
+        $error .= '<div class="tab-content">
+                      <div class="tab-pane active" id="general">[GENERAL_ERR]</div> 
+                      <div class="tab-pane" id="request">[REQUEST_ERR]</div> 
+                      <div class="tab-pane" id="router">[ROUTER_ERR]</div> 
+                      <div class="tab-pane" id="modules">[MODULES_ERR]</div>
+                      <div class="tab-pane" id="config">[CONFIG_ERR]</div>
+                      <div class="tab-pane" id="get">[GET_ERR]</div>
+                      <div class="tab-pane" id="post">[POST_ERR]</div>
+                      <div class="tab-pane" id="cookie">[COOKIE_ERR]</div>
+                      <div class="tab-pane" id="session">[SESSION_ERR]</div>
+                      <div class="tab-pane" id="server">[SERVER_ERR]</div>
+                      <div class="tab-pane" id="sql">[SQL_ERR]</div>
+                    </div>';
 
-            $search  = array('CSS_PATH', 'JS_PATH');
-            $replace = array(CSS_PATH, JS_PATH);
-            $error = str_replace($search, $replace, $error);
+        $search  = array('CSS_PATH', 'JS_PATH');
+        $replace = array(CSS_PATH, JS_PATH);
+        $error = str_replace($search, $replace, $error);
 
-            // Environ
-            $environ = Yaf_Application::app()->environ();
+        // Environ
+        $environ = Yaf_Application::app()->environ();
 
-            // General
-            $generalErr = '<ul>';
-            $generalErr .= '<li>Environ: '.$environ.'</li>';
-            $generalErr .= '<li>Error NO: '.$errno.'</li>';
-            $generalErr .= '<li>Error: '.$errstr.'</li>';
-            $generalErr .= '<li>File: '.$errfile.'</li>';
-            $generalErr .= '<li>Line: '.$errline.'</li>';
-            $generalErr .= '<li>URL: http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'</li>';
-            $generalErr .= '</ul>';
+        // General
+        $generalErr = '<ul>';
+        $generalErr .= '<li>Environ: '.$environ.'</li>';
+        $generalErr .= '<li>Error NO: '.$errno.'</li>';
+        $generalErr .= '<li>Error: '.$errstr.'</li>';
+        $generalErr .= '<li>File: '.$errfile.'</li>';
+        $generalErr .= '<li>Line: '.$errline.'</li>';
+        $generalErr .= '<li>URL: http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'</li>';
+        $generalErr .= '</ul>';
 
-            $error = str_replace('[ERROR_DESC]', $errstr, $error);
-            $error = str_replace('[GENERAL_ERR]', $generalErr, $error);
+        $error = str_replace('[ERROR_DESC]', $errstr, $error);
+        $error = str_replace('[GENERAL_ERR]', $generalErr, $error);
 
-            // Request
-            $request = Yaf_Dispatcher::getInstance()->getRequest();
-            $requestErr = '<ul>';
-            $requestErr .= '<li>Module: '.$request->module.'</li>';
-            $requestErr .= '<li>Controller: '.$request->controller.'</li>';
-            $requestErr .= '<li>Action: '.$request->action.'</li>';
-            $requestErr .= '<li>URI: '.$request->getRequestUri().'</li>';
-            $requestErr .= '</ul>';
+        // Request
+        $request = Yaf_Dispatcher::getInstance()->getRequest();
+        $requestErr = '<ul>';
+        $requestErr .= '<li>Module: '.$request->module.'</li>';
+        $requestErr .= '<li>Controller: '.$request->controller.'</li>';
+        $requestErr .= '<li>Action: '.$request->action.'</li>';
+        $requestErr .= '<li>URI: '.$request->getRequestUri().'</li>';
+        $requestErr .= '</ul>';
 
-            $error = str_replace('[REQUEST_ERR]', $requestErr, $error);
+        $error = str_replace('[REQUEST_ERR]', $requestErr, $error);
 
-            // Routers
-            $router = Yaf_Dispatcher::getInstance()->getRouter();
-            $routers = $router->getRoutes();
+        // Routers
+        $router = Yaf_Dispatcher::getInstance()->getRouter();
+        $routers = $router->getRoutes();
 
-            // if($routers){
-            //     Helper::import('Array');
-            //     foreach($routers as $key => $val){
-            //         // $val = object2Array($val);
-            //         pr($key);
-            //         pr($val); continue;
-            //     }
-            // }
+        // if($routers){
+        //     Helper::import('Array');
+        //     foreach($routers as $key => $val){
+        //         // $val = object2Array($val);
+        //         pr($key);
+        //         pr($val); continue;
+        //     }
+        // }
 
-            // Current Router
-            $currentRouter = $router->getCurrentRoute();
-            $routerErr = '<ul>';
-            $routerErr .= '<li>Current Router: '.$currentRouter.'</li>';
-            $routerErr .= '</ul>';
-            $error = str_replace('[ROUTER_ERR]', $routerErr, $error);
+        // Current Router
+        $currentRouter = $router->getCurrentRoute();
+        $routerErr = '<ul>';
+        $routerErr .= '<li>Current Router: '.$currentRouter.'</li>';
+        $routerErr .= '</ul>';
+        $error = str_replace('[ROUTER_ERR]', $routerErr, $error);
 
-            // Modules
-            $modules = Yaf_Application::app()->getModules();
+        // Modules
+        $modules = Yaf_Application::app()->getModules();
 
-            $moduleErr = '<ul>';
-            foreach($modules as $val){
-                $moduleErr .= '<li>'.$val.'</li>';
-            }
-            $moduleErr .= '</ul>';
-
-            $error = str_replace('[MODULES_ERR]', $moduleErr, $error);
-
-            // Config
-            $config = Yaf_Registry::get('config');
-            $configErr = '<ul>';
-            foreach($config as $key => $val){
-                if($key != 'application'){
-                    // Hide PSWD of MySQL
-                    if(strpos($key, 'PSWD') !== FALSE){
-                        $val = '******';
-                    }   
-                    $configErr .= '<li>'.$key. ' => '.$val.'</li>';
-                }
-            }
-            $configErr .= '</ul>';
-            $error = str_replace('[CONFIG_ERR]', $configErr, $error);
-
-            // $_GET
-            $getErr = '<ul>';
-            foreach($_GET as $key => $val){
-                $getErr .= '<li>'.$key. ' => '.$val.'</li>';
-            }
-            $getErr .= '</ul>';
-            $error = str_replace('[GET_ERR]', $getErr, $error);
-
-            // $_POST
-            $postErr = '<ul>';
-            foreach($_POST as $key => $val){
-                $postErr .= '<li>'.$key. ' => '.$val.'</li>';
-            }
-            $postErr .= '</ul>';
-            $error = str_replace('[POST_ERR]', $postErr, $error);
-
-            // $_COOKIE
-            $cookieErr = '<ul>';
-            foreach($_COOKIE as $key => $val){
-                $cookieErr .= '<li>'.$key. ' => '.$val.'</li>';
-            }
-            $cookieErr .= '</ul>';
-            $error = str_replace('[COOKIE_ERR]', $cookieErr, $error);
-
-            // $_SESSION
-            $sessionErr = '<ul>';
-            if($_SESSION){
-                foreach($_SESSION as $key => $val){
-                    $sessionErr .= '<li>'.$key. ' => '.$val.'</li>';
-                }
-            }
-            $sessionErr .= '</ul>';
-            $error = str_replace('[SESSION_ERR]', $sessionErr, $error);
-
-            // $_SERVER
-            $serveErr = '<ul>';
-            foreach($_SERVER as $key => $val){
-                $serveErr .= '<li>'.$key. ' => '.$val.'</li>';
-            }
-            $serveErr .= '</ul>';
-            $error = str_replace('[SERVER_ERR]', $serveErr, $error);
-
-            // SQL
-            if($sql){
-                $sqlErr = '<ul>';
-                $sqlErr .= '<li>'.$sql.'</li>';
-                $sqlErr .= '</ul>';
-                $error = str_replace('[SQL_ERR]', $sqlErr, $error);
-            }
-
-            echo $error; die;
-        }else{
-            return TRUE;
+        $moduleErr = '<ul>';
+        foreach($modules as $val){
+            $moduleErr .= '<li>'.$val.'</li>';
         }
+        $moduleErr .= '</ul>';
+
+        $error = str_replace('[MODULES_ERR]', $moduleErr, $error);
+
+        // Config
+        $config = Yaf_Registry::get('config');
+        $configErr = '<ul>';
+        foreach($config as $key => $val){
+            if($key != 'application'){
+                // Hide PSWD of MySQL
+                if(strpos($key, 'PSWD') !== FALSE){
+                    $val = '******';
+                }   
+                $configErr .= '<li>'.$key. ' => '.$val.'</li>';
+            }
+        }
+        $configErr .= '</ul>';
+        $error = str_replace('[CONFIG_ERR]', $configErr, $error);
+
+        // $_GET
+        $getErr = '<ul>';
+        foreach($_GET as $key => $val){
+            $getErr .= '<li>'.$key. ' => '.$val.'</li>';
+        }
+        $getErr .= '</ul>';
+        $error = str_replace('[GET_ERR]', $getErr, $error);
+
+        // $_POST
+        $postErr = '<ul>';
+        foreach($_POST as $key => $val){
+            $postErr .= '<li>'.$key. ' => '.$val.'</li>';
+        }
+        $postErr .= '</ul>';
+        $error = str_replace('[POST_ERR]', $postErr, $error);
+
+        // $_COOKIE
+        $cookieErr = '<ul>';
+        foreach($_COOKIE as $key => $val){
+            $cookieErr .= '<li>'.$key. ' => '.$val.'</li>';
+        }
+        $cookieErr .= '</ul>';
+        $error = str_replace('[COOKIE_ERR]', $cookieErr, $error);
+
+        // $_SESSION
+        $sessionErr = '<ul>';
+        if($_SESSION){
+            foreach($_SESSION as $key => $val){
+                $sessionErr .= '<li>'.$key. ' => '.$val.'</li>';
+            }
+        }
+        $sessionErr .= '</ul>';
+        $error = str_replace('[SESSION_ERR]', $sessionErr, $error);
+
+        // $_SERVER
+        $serveErr = '<ul>';
+        foreach($_SERVER as $key => $val){
+            $serveErr .= '<li>'.$key. ' => '.$val.'</li>';
+        }
+        $serveErr .= '</ul>';
+        $error = str_replace('[SERVER_ERR]', $serveErr, $error);
+
+        // SQL
+        if($sql){
+            $sqlErr = '<ul>';
+            $sqlErr .= '<li>'.$sql.'</li>';
+            $sqlErr .= '</ul>';
+            $error = str_replace('[SQL_ERR]', $sqlErr, $error);
+        }
+
+        echo $error; die;
+
     }
 }
