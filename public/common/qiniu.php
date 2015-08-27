1379c1aa74e22ec614c4773c55ac901245c7adfe
System Administrator <root@userdeMacBook-Pro.local>
2015-08-27 15:22:56 +0800

    Add yaf.library to autoload

--- public/common/qiniu.php
+++ public/common/qiniu.php
@@ -3,23 +3,19 @@
 $avatar = $_FILES['avatar'];
 
 if($avatar['name']){
-    Yaf_Loader::import('L_Upload.class.php');
-    
     $dir = TMP_PATH.'/';
             
     $fileName = date('YmdHis');
-    $upload = new L_Upload($avatar, $dir);
+    $upload = new Upload($avatar, $dir);
 
     $code = $upload->upload($fileName);
 
     if($code == 1){
-		Yaf_Loader::import('L_Qiniu.class.php');
-
 		$img = $fileName . '.' . $upload->extension;
         $key = time().'.'.$upload->extension;
         $source = $dir.$img;
 
-        $l_qiniu = new L_Qiniu();
+        $l_qiniu = new Qiniu();
         list($ret, $err) = $l_qiniu->upload($key, $source);
 
         if($err !== NULL){
