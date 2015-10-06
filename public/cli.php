8b248a763d9277034a1f421de5b3e46b4daf1d1f
System Administrator <root@userdeMacBook-Pro.local>
2015-10-07 00:30:03 +0800

    Add cli mode

--- public/cli.php
+++ public/cli.php
@@ -9,22 +9,5 @@ define('APP_PATH',  realpath(dirname(__FILE__) . '/../'));
 
 Yaf_Loader::import(APP_PATH.'/application/init.php');
 
-/*
- * callback 为回调函数, $argc 为参数总数, $argv 为可变的参数列表
- * 可根据 $argv 来作不同的业务处理
- * callback 中模型的加载请使用 Helper::load($model), 其余的和 WEB 一样
- */
-
 $app = new Yaf_Application(APP_PATH.'/conf/application.ini');
-$app->bootstrap()->execute('callback', $argc, $argv);
-
-/*
- * 示例: 根据用户 ID 查询用户资料, 命令如下
- * php cli.php 9
- */
-function callback($argc, $argv) {
-	$m_user = Helper::load('User');
-	$field  = array('id', 'username', 'realname', 'province', 'city', 'region');
-	$users  = $m_user->SelectByID($field, $argv[1]);
-	pr($users);
-}
+$app->bootstrap()->getDispatcher()->dispatch(new Yaf_Request_Simple());
