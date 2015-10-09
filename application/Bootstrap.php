d9b5336808ca6b9b9f01f1dc6687669a6ad2e495
System Administrator <root@userdeMacBook-Pro.local>
2015-10-10 00:26:07 +0800

    Remove inifConfig function

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,33 +2,8 @@
 
 class Bootstrap extends Yaf_Bootstrap_Abstract{
 
-    // Init config
-    public function _initConfig() {
-        $config = Yaf_Application::app()->getConfig();
-        Yaf_Registry::set('config', $config);
-    }
-
     // Load libaray, MySQL model, function
     public function _initCore() {
-        define('YOF_VERSION',  '2.0'); // YOF VERSION
-        define('TB_PK',        'id');  // 表的主键, 用于 SelectByID 等
-        define('TB_PREFIX',    'zt_'); // 表前缀
-        define('APP_NAME',     'YOF-DEMO');
-        define('LIB_PATH',     APP_PATH.'/application/library');
-        define('CORE_PATH',    LIB_PATH.'/core');
-        define('MODEL_PATH',   APP_PATH.'/application/model');
-        define('FUNC_PATH',    APP_PATH.'/application/function');
-        define('ADMIN_PATH',   APP_PATH.'/application/modules/Admin');
-
-        // CSS, JS, IMG PATH
-        define('CSS_PATH', '/css');
-        define('JS_PATH',  '/js');
-        define('IMG_PATH', '/img');
-
-        // Admin CSS, JS PATH
-        define('ADMIN_CSS_PATH', '/admin/css');
-        define('ADMIN_JS_PATH',  '/admin/js');
-
         // 设置自动加载的目录
         ini_set('yaf.library', LIB_PATH);
         
@@ -41,12 +16,6 @@ class Bootstrap extends Yaf_Bootstrap_Abstract{
         // 导入 F_Basic.php 与 F_Network.php
         Helper::import('Basic');
         Helper::import('Network');
-
-        // COMMON_PATH
-        define('COMMON_PATH', APP_PATH.'/public/common');
-
-        // API KEY for api sign
-        define('API_KEY', 'THIS_is_OUR_API_keY');
     }
 
     // 这里我们添加三种路由，分别为 rewrite, rewrite_category, regex
