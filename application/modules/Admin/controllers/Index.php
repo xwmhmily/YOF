59b1810e97dea82472f4310a3ea14294073b2bd9
System Administrator <root@userdeMacBook-Pro.local>
2015-11-29 14:10:54 +0800

    Refactor this controller

--- application/modules/Admin/controllers/Index.php
+++ application/modules/Admin/controllers/Index.php
@@ -5,43 +5,11 @@ class IndexController extends BasicController {
   	private $adminAccount = 'superAdmin';
 
 	private function init(){
-		
-	}
-
-	public function mainAction(){
-		include APP_PATH.'/application/modules/Admin/menu.php';
-		if($this->adminAccount != $this->getSession('adminName')){
-			$priv = $this->getSession('priv');
-			$priv = explode(',', $priv['privilege']);
-			
-			// 1: 与大菜单对比, 删除会员没有权限的菜单
-			foreach($menu as $k => $v){
-				foreach($v as $kk => $vv){
-					if(is_array($vv)){
-						foreach($vv as $kkk => $vvv){
-							if(!in_array($kkk, $priv)){
-								unset($menu[$k][$kk][$kkk]);
-							}
-						}
-					}
-				}
-			}
-			
-			// 2: 进一步处理: 删除没有子菜单的项
-			foreach($menu as $k => $v){
-				if(!$v['sub']){
-					unset($menu[$k]);
-				}
-			}
-		}
-
-		$buffer['menu'] = $menu;
-		$this->setSession('menu', $menu);
-		$this->getView()->assign($buffer);
+		Yaf_Registry::get('adminPlugin')->checkLogin();
 	}
 
 	public function resetAction(){
-
+		
 	}
 
 	public function resetActAction(){
