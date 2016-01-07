0526900a62e1bfa8d97aaa0571de9fedf923f543
System Administrator <root@userdeMacBook-Pro.local>
2016-01-07 15:19:25 +0800

    Upgrade captcha generator

--- public/captcha.php
+++ public/captcha.php
@@ -4,35 +4,47 @@
  * 
  * Remark: 接收 t 这个参数，可以生成多个验证码
  */
- 
-session_start();
 
 $t = $_GET['t'];
 
-$num = 4;     //验证码个数
-$width = 80;  //验证码宽度
-$height = 25; //验证码高度
-$code = ' ';
+$imgwidth  = 100; //图片宽度
+$imgheight = 40; //图片高度
+$codelen   = 4; //验证码长度
+$fontsize  = 20; //字体大小
+$charset   = 'abcdefghkmnprstuvwxyzABCDEFGHKMNPRSTUVWXYZ23456789';
+$font      = '/asset/msyh.ttf';
+$im = imagecreatetruecolor($imgwidth, $imgheight);
+$while = imageColorAllocate($im, 255, 255, 255);
+imagefill($im, 0, 0, $while); //填充图像
 
-for($i = 0; $i < $num; $i++){//生成验证码
-	$code[$i] = chr(rand(65, 90));
+//取得字符串
+$authstr = '';
+$_len = strlen($charset)-1;
+for ($i = 0; $i < $codelen; $i++) {
+ 	$authstr .= $charset[mt_rand(0, $_len)];
 }
 
-$_SESSION[$t."Captcha"] = $code;
-$image = imagecreate($width, $height);
-imagecolorallocate($image, 255, 255, 255);
-
-for($i = 0; $i < 80; $i++) {//生成干扰像素
-	$dis_color = imagecolorallocate($image, rand(0, 2555), rand(0, 255), rand(0, 255));
-	imagesetpixel($image, rand(1, $width), rand(1, $height), $dis_color);
+session_start();
+$_SESSION[$t."Captcha"] = strtolower($authstr);
+for ($i = 0; $i < $imgwidth; $i++){
+    $randcolor = imageColorallocate($im, mt_rand(200, 255), mt_rand(200, 255), mt_rand(200, 255));
+ 	imagestring($im, mt_rand(1, 5), mt_rand(0, $imgwidth), mt_rand(0, $imgheight), '*', $randcolor);
 }
 
-for($i = 0; $i < $num; $i++) {//打印字符到图像
-	$char_color = imagecolorallocate($image, 55, 88, 99);
-	imagechar($image, 60, ($width / $num) * $i, rand(0, 5), $code[$i], $char_color);
+//随机画线,线条数量=字符数量（随便）
+for($i = 0; $i < $codelen; $i++) {  
+	$randcolor = imagecolorallocate($im, mt_rand(0,255), mt_rand(0, 255), mt_rand(0, 255));
+ 	imageline($im, 0, mt_rand(0, $imgheight), $imgwidth, mt_rand(0, $imgheight), $randcolor); 
 }
 
-header("Content-type:image/png");
+$_x = intval($imgwidth / $codelen); //计算字符距离
+$_y = intval($imgheight * 0.7); //字符显示在图片70%的位置
+for($i = 0; $i < strlen($authstr); $i++){
+ 	$randcolor = imagecolorallocate($im, mt_rand(0, 150), mt_rand(0, 150), mt_rand(0, 150));
+ 	imagettftext($im, $fontsize, mt_rand(-30, 30), $i * $_x + 3, $_y, $randcolor, $font, $authstr[$i]);
+}
 
-imagepng($image);
-imagedestroy($image);
+//生成图像
+header("content-type:image/PNG");
+imagePNG($im);
+imageDestroy($im);
